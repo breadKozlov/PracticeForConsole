c8806a78f38354d4d90a1f2507dbb4ab9c51a4cf
breadKozlov <killersarequiet@gmail.com>
2022-10-20 19:16:25 +0300

    Hello

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
+package practice.PracticeForConsole;
+
 public class Main {
     public static void main(String[] args) {
+
+
         System.out.println("Hello world! Hi, Pasha");
+        System.out.println("My name is Pasha");
     }
 }
